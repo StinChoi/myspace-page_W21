4cff401ef9caa8dfc34b80cdef42344505dccbea
Austin <austinchoi22@gmail.com>
2021-12-29 22:46:54 -0700

    made some changes to createpost in migration

--- db/migrate/20211230052538_create_posts.rb
+++ db/migrate/20211230052538_create_posts.rb
@@ -1,6 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
+      t.text :content
+      t.integer :likes
+      t. belongs_to :user, null: false, foreign_key: true
 
       t.timestamps
     end
